bb51106024721ebb481cf7041b4d9220b76d2b15
m-fudala <michfudala@gmail.com>
2024-03-01 09:00:33 +0100

    Bus status as bit field Interrupt 6 added to bus Option to set device as initiator or follower

--- hdmi_cec.c
+++ hdmi_cec.c
@@ -1,27 +1,49 @@
 #include "hdmi_cec.h"
 
-Pin CEC_bus;
-
 void cec_init() {
-    bit_sent = 1;
+    CEC_status.bit_sent = 1;
+    CEC_status.bus_direction = FOLLOWER;
 
     cli();
 
     // init pin
-    pin_init(&CEC_bus, &PORTE, PE6, OUTPUT);
-    pin_write(&CEC_bus, HIGH);
+    pin_init(&CEC_bus, &PORTE, PE6, INPUT);
+    // pin_write(&CEC_bus, HIGH);
 
     // init timer 1
     TIMSK1 |= _BV(OCIE1A) | _BV(OCIE1B);
 
+    // init interrupt 6
+    int_init(INT6, bus_interrupt_handler, FALLING_EDGE);
+    int_enable(INT6);
+
     sei();
 }
 
+void set_initiator() {
+    int_disable(INT6);
+
+    pin_set_direction(&CEC_bus, INPUT_PULLUP);  // intermediate step
+    pin_set_direction(&CEC_bus, OUTPUT);
+
+    CEC_status.bus_direction = INITIATOR;
+}
+
+void set_follower() {
+    pin_set_direction(&CEC_bus, INPUT);
+
+    int_enable(INT6);
+
+    CEC_status.bus_direction = FOLLOWER;
+}
+
 void send_start() {
+    set_initiator();
+
     OCR1A = START_LOW_TIME;
     OCR1B = START_HIGH_TIME;
 
-    bit_sent = 0;
+    CEC_status.bit_sent = 0;
 
     TCNT1 = 0;
     TCCR1B |= _BV(CS11);
@@ -38,7 +60,7 @@ void send_bit(unsigned char bit) {
 
     OCR1B = BIT_HIGH_TIME;
 
-    bit_sent = 0;
+    CEC_status.bit_sent = 0;
 
     TCNT1 = 0;
     TCCR1B |= _BV(CS11);
@@ -49,21 +71,25 @@ void send_bit(unsigned char bit) {
 void send_byte(unsigned char byte) {
     send_start();
 
-    while (!bit_sent);
+    while (!CEC_status.bit_sent);
 
     for (unsigned char bit_id = 0; bit_id < 8; ++bit_id) {
         send_bit((byte & (0x80 >> bit_id)) >> (7 - bit_id));
 
-        while (!bit_sent);
+        while (!CEC_status.bit_sent);
     }
 
     send_bit(1);
 
-    while (!bit_sent);
+    while (!CEC_status.bit_sent);
 
     send_bit(1);
 
-    while (!bit_sent);
+    while (!CEC_status.bit_sent);
+}
+
+void bus_interrupt_handler() {
+
 }
 
 ISR (TIMER1_COMPA_vect) {
@@ -73,5 +99,5 @@ ISR (TIMER1_COMPA_vect) {
 ISR (TIMER1_COMPB_vect) {
     TCCR1B &= ~_BV(CS11);
 
-    bit_sent = 1;
+    CEC_status.bit_sent = 1;
 }
--- hdmi_cec.h
+++ hdmi_cec.h
@@ -11,6 +11,7 @@
 #include <avr/interrupt.h>
 
 #include "ATmega32u4_libraries/gpio_library/gpio.h"
+#include "ATmega32u4_libraries/external_interrupt_library/external_interrupt.h"
 
 #define START_LOW_TIME 7400
 #define START_HIGH_TIME 9000
@@ -18,14 +19,31 @@
 #define ZERO_LOW_TIME 3000
 #define BIT_HIGH_TIME 4800
 
-volatile unsigned char bit_sent;
+enum CEC_direction {
+    INITIATOR,
+    FOLLOWER
+};
+
+typedef struct CEC {
+    volatile unsigned char bit_sent : 1;
+    volatile unsigned char bus_direction : 1; 
+} CEC;
+
+Pin CEC_bus;
+CEC CEC_status;
 
 void cec_init();
+
+void set_initiator();
+
+void set_follower();
 
 void send_start();
 
 void send_bit(unsigned char bit);
 
 void send_byte(unsigned char byte);
+
+void bus_interrupt_handler();
 
 #endif
